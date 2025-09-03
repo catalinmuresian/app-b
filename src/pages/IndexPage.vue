d6ab6bed9c44ff9cd020cbfc9cb9ada35761faae
catalinmuresian <82952859+catalinmuresian@users.noreply.github.com>
2025-09-03 16:04:49 +0300

    deploy

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,16 @@
 <template>
   <q-page class="flex flex-center">
     <h1>APP - B</h1>
+    <q-btn @click="handleBtn">nav to APP - A</q-btn>
   </q-page>
 </template>
 
 <script setup>
-//
+import {useRouter} from "vue-router";
+
+const router = useRouter()
+function handleBtn () {
+  router.push('https://catalinmuresian.github.io/app-a/')
+}
+
 </script>
