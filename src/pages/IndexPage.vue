b06eafcae4aa960b271e5a5810454498def93b47
catalinmuresian <82952859+catalinmuresian@users.noreply.github.com>
2025-09-03 16:30:51 +0300

    deploy

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -10,7 +10,7 @@ import {useRouter} from "vue-router";
 
 const router = useRouter()
 function handleBtn () {
-  window.open('https://catalinmuresian.github.io/app-a/', '_blank');
+  window.location.href = 'https://catalinmuresian.github.io/app-a/'
 }
 
 </script>
